d2ee5bcfef5f4844b49890f0a044d30245bdf195
lgabor85 <laagabinl@gmail.com>
2024-08-04 11:37:01 +0200

    Add code for practicing string and numerical operations

---
+++ CS/python/varpractice.py
@@ -0,0 +1,23 @@
+print ("-----------------Practice 1-----------------")
+var1="hear"
+print(var1,var1)
+print(var1,var1,var1)
+
+print("\n") # new line
+
+print ("-----------------Practice 2-----------------")
+message = "move slowly"
+message = "and"
+message = "fix things"
+print(message)
+
+print("\n") # new line
+
+print ("-----------------Practice 3-----------------")
+num1 = 4
+num2 = 6
+num3 = num1 * num2
+print(num3)
+
+print("\n") # new line
+
