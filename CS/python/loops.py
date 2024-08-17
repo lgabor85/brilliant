59f908f2db921633c2783ec7c3162a33a05d5548
lgabor85 <laagabinl@gmail.com>
2024-08-17 13:11:21 +0200

    Add for loop examples to loops.py

---
+++ CS/python/loops.py
@@ -0,0 +1,17 @@
+print ("================|For Loops|================")
+print ("\n") # new line
+print ("--------------|Example 1|--------------")
+
+for i in range(5):
+    print("Hello World!") # add a message here
+
+print("\n") # new line
+
+print ("--------------|Example 2|--------------")
+
+number = 5               # start with 5
+for i in range(3):       # do this 3 times
+    number = number + 2  # add 2 to the number
+print(number)
+
+print("\n") # new line
