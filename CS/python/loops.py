46fcbc86dfb457d1b7c5325c097bc506771d9cba
lgabor85 <laagabinl@gmail.com>
2024-08-18 12:54:43 +0200

    Add examples 3, 4, and 5 to loops.py

--- CS/python/loops.py
+++ CS/python/loops.py
@@ -15,3 +15,30 @@ for i in range(3):       # do this 3 times
 print(number)
 
 print("\n") # new line
+
+print ("--------------|Example 3|--------------")
+
+print("You say goodbye")
+print("and I say hello")
+for i in range(3):      # do this 3 times
+    print("Hello")      # this is in the loop
+    print("Goodbye")    # this is also in the loop
+print("Goodby forever") # this is outside the loop
+
+print("\n") # new line
+
+print ("--------------|Example 4|--------------")
+
+for number in range(5):
+    print(number)
+
+print("\n") # new line
+
+print ("--------------|Example 5|--------------")
+
+number = 0                  # start with 0   
+for letter in "string":     # for each letter in the word "string"
+    number = number + 1     # add 1 to the number
+    print(letter, number)   # print the letter and the number
+
+print("\n") # new line
