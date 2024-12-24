022a3abdc23fa1972671b77920a1d157981b4df3
Gabor <laagabinl@gmail.com>
2024-12-24 09:04:37 +0000

    new pyscript draw_turtle.py

---
+++ CS/python/draw_turtle.py
@@ -0,0 +1,14 @@
+from turtle import * # Loads commands used to draw
+
+color('red') # Set the color of the pen
+forward(200) # Move the turtle forward by 200 units
+left(90) # Turn the turtle left by 90 degrees
+# turtle will now be facing up
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+
+done() # Finish drawing
